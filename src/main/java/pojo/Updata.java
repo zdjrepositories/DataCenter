2a855c7a3b579590f728178270641ff399864611
张冬军 <zdjyx126@126.com>
2019-12-03 14:27:54 +0800

    V0.0.6

---
+++ src/main/java/pojo/Updata.java
@@ -0,0 +1,12 @@
+package pojo;
+
+public class Updata {
+    private int id;
+    private String date;
+    private int bu;
+    private int cat;
+    private int subcat;
+    private int range;
+    private int prducts;
+
+}
